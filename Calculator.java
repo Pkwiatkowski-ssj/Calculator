64da84c34ac69029ca6a6789328cf8c6a3ab80dc
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-02-06 10:15:58 -0500

    Included a prompt to ask the user if they'd like to perform addition or subtraction with the two numbers.

--- Calculator.java
+++ Calculator.java
@@ -3,14 +3,25 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter two numbers please ");
-
         int num1 = input.nextInt();
         int num2 = input.nextInt();
 
-        int sum = num1 + num2;
+        input.nextLine();
+        System.out.print("Would you like to add or subtract these numbers? ");
+        
+        String operation = input.nextLine().toLowerCase();
 
-        System.out.println("The sum of " + num1 + " and " +num2 + " is " +sum);
-
+        if (operation.equals("add")) {
+            int sum = num1 +num2;
+            System.out.println("You chose to add " +num1 + " and " + num2 + " is: " + sum);
+        }else if (operation.equals("subtract")){
+            int difference = num1 - num2;
+            System.out.println("You chose subtraction. The difference between " + num1 + " and " + num2 + " is: " + difference);
+        } else {
+            System.out.println(" Invalid choice. Please type 'add' or 'subtract'.");
+        }
         input.close();
+        
+        
     }
 }
